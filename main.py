d1daae72e078ba83f7a270ff7eca8fe8e95a9aad
Hrishi Raj Sonar <73255908+hrisshizz@users.noreply.github.com>
2022-02-08 00:00:47 +0545

    main.py added

---
+++ main.py
@@ -0,0 +1,22 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import numpy as np
+from numpy import ndarray
+
+import add  #this will run everything executed outside of main of add.py
+
+def factorial():
+    num1 = np.array([1,2,3])
+    print(num1)
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    #print(factorial(4))
+    factorial()
+    factorial()
+    print(add.add(1,2))
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
